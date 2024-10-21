cf13104f24d431369ff000d2d256d02eca8c3d10
RicarGit <ricardoagava@gmail.com>
2024-10-21 20:26:53 -0300

    feat: creating type and fn to create an event

--- src/services/events.ts
+++ src/services/events.ts
@@ -1,4 +1,7 @@
+import { Prisma } from "@prisma/client"
 import client from "../lib/prisma-client"
+
+type CreateEventPayload = Pick<Prisma.EventCreateInput, 'title' | 'description'>
 
 export const getAll = () => {
   const response = client.event.findMany()
@@ -12,6 +15,14 @@ export const getOne = (id: number) => {
   const response = client.event.findUniqueOrThrow({ where: { id } })
     .then(data => ({ status: 200, data }))
     .catch(() => ({ status: 404, data: { error: 'Id não encontrado.' } }))
+
+  return response
+}
+
+export const create = (event: CreateEventPayload) => {
+  const response = client.event.create({ data: event })
+    .then(data => ({ status: 200, data }))
+    .catch(() => ({ status: 400, data: { error: 'Ocorreu um erro ao cadastrar.' } }))
 
   return response
 }
