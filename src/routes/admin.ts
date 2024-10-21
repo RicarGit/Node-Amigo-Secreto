6d719756bc9c106573db7e116de7066dc2cb8081
RicarGit <ricardoagava@gmail.com>
2024-10-21 20:24:40 -0300

    feat: creating single event route

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -7,5 +7,6 @@ const router = Router()
 router.get('/ping', auth.validate, (_, res) => res.json({ pong: true, admin: true }))
 router.get('/events', auth.validate, event.getAll)
 router.post('/login', auth.login)
+router.get('/events/:id', auth.validate, event.getOneEvent)
 
 export default router
