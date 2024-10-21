e6972057bab000566851eb41185d62b08853579e
RicarGit <ricardoagava@gmail.com>
2024-10-21 20:22:21 -0300

    feat: creating fn to fetch single event

--- src/services/events.ts
+++ src/services/events.ts
@@ -7,3 +7,11 @@ export const getAll = () => {
 
   return response
 }
+
+export const getOne = (id: number) => {
+  const response = client.event.findUniqueOrThrow({ where: { id } })
+    .then(data => ({ status: 200, data }))
+    .catch(() => ({ status: 404, data: { error: 'Id não encontrado.' } }))
+
+  return response
+}
