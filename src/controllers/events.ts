1e429803349309cfb91f2ffee3a4578d2dc3eb08
RicarGit <ricardoagava@gmail.com>
2024-10-21 20:33:54 -0300

    feat: creating a create fn controller and validate schema

--- src/controllers/events.ts
+++ src/controllers/events.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express"
 import * as events from '../services/events'
+import { z } from "zod"
 
 export const getAll: RequestHandler = async (_, res) => {
   const { status, data } = await events.getAll()
@@ -14,3 +15,20 @@ export const getOneEvent: RequestHandler = async (req, res) => {
   res.status(status).json({ ...data })
 }
 
+export const createEvent: RequestHandler = async (req, res) => {
+  const createEventSchema = z.object({
+    title: z.string(),
+    description: z.string()
+  })
+
+  const parsedBody = createEventSchema.safeParse(req.body)
+
+  if (!parsedBody.success) return res.status(400).json({ error: 'Dados inválidos.' })
+
+  const { title, description } = parsedBody.data
+  const newEvent = await events.create({ title, description })
+
+  if (!newEvent) return res.status(400).json({ error: 'Ocorreu um erro.' })
+
+  res.status(201).json({ message: 'Evento criado com sucesso.', event: newEvent })
+}
