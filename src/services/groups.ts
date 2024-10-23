e8b7782bb9dbd316f12aabdaff3ca62a72fb8254
RicarGit <ricardoagava@gmail.com>
2024-10-22 23:13:12 -0300

    feat: creating fn to fetch single group

--- src/services/groups.ts
+++ src/services/groups.ts
@@ -1,9 +1,22 @@
 import client from "../lib/prisma-client"
 
+type GroupIds = {
+  id: number
+  event_id?: number
+}
+
 export const getAll = (event_id: number) => {
   const response = client.event_Group.findMany({ where: { event_id } })
     .then(data => ({ status: 200, data }))
     .catch(() => ({ status: 404, data: { error: 'Ocorreu um erro.' } }))
+
+  return response
+}
+
+export const getOne = ({ id, event_id }: GroupIds) => {
+  const response = client.event_Group.findFirstOrThrow({ where: { OR: [{ id }, { event_id }] } })
+    .then(data => ({ status: 200, data }))
+    .catch(() => ({ status: 404, data: { error: 'Grupo não encontrado.' } }))
 
   return response
 }
