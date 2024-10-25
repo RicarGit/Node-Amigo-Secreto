0b2ead02c7484e0a82f0e66bfff5354456d6fea4
RicarGit <ricardoagava@gmail.com>
2024-10-24 21:21:24 -0300

    feat: creating fn controller to create an event group

--- src/controllers/groups.ts
+++ src/controllers/groups.ts
@@ -33,3 +33,26 @@ export const getOneGroup: RequestHandler = async (req, res) => {
 
   res.status(status).json({ ...data })
 }
+
+export const createGroup: RequestHandler = async (req, res) => {
+  const createEventGroupParamsSchema = z.object({
+    event_id: z.coerce.number()
+  })
+
+  const createEventGroupBodySchema = z.object({
+    name: z.string()
+  })
+
+  const parsedParams = createEventGroupParamsSchema.safeParse(req.params)
+  const parsedBody = createEventGroupBodySchema.safeParse(req.body)
+
+  if (!parsedParams.success || !parsedBody.success) return res.status(400).json({ error: 'Dados inválidos.' })
+
+  const { event_id } = parsedParams.data
+  const { name } = parsedBody.data
+  const { status, data } = await groups.create({ event_id, name })
+
+  if (status === 400) return res.status(status).json({ ...data })
+
+  res.status(status).json({ ...data })
+}
