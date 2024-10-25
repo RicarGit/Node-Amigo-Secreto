a87c672e943abedcd8b98b144b0d9ba42f9f6dd1
RicarGit <ricardoagava@gmail.com>
2024-10-24 23:57:55 -0300

    feat: creating fn to update an event group

--- src/services/groups.ts
+++ src/services/groups.ts
@@ -11,6 +11,12 @@ type CreateGroupPayload = {
   name: string
 }
 
+type UpdateGroupPayload = {
+  id: number
+  event_id: number
+  name: string
+}
+
 export const getAll = (event_id: number) => {
   const response = client.event_Group.findMany({ where: { event_id } })
     .then(data => ({ status: 200, data }))
@@ -35,6 +41,14 @@ export const create = async ({ event_id, name }: CreateGroupPayload) => {
   const response = client.event_Group.create({ data: { event_id, name } })
     .then(data => ({ status: 201, data }))
     .catch(() => ({ status: 400, data: { error: 'Ocorreu um erro ao cadastrar.' } }))
+
+  return response
+}
+
+export const update = ({ id, event_id, name }: UpdateGroupPayload) => {
+  const response = client.event_Group.update({ where: { id, AND: { event_id } }, data: { name } })
+    .then(data => ({ status: 200, data }))
+    .catch(() => ({ status: 400, data: { error: 'Ocorreu um erro ao atualizar.' } }))
 
   return response
 }
