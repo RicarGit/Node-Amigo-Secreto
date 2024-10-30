15da2cdc4e4ef1898148ac0a3f41dc1b66f39836
RicarGit <ricardoagava@gmail.com>
2024-10-29 23:39:01 -0300

    feat: creating fn to update a participant or many participants

--- src/services/participants.ts
+++ src/services/participants.ts
@@ -18,6 +18,15 @@ type CreateParticipantPayload = {
   cpf: string
 }
 
+type UpdateParticipantPayload = {
+  id?: number
+  event_id: number
+  event_group?: number
+  name?: string
+  cpf?: string
+  matched?: string
+}
+
 export const getAll = ({ event_id, event_group }: ParticipantsIDs) => {
   const response = client.participant.findMany({ where: { event_id, event_group }, orderBy: { id: 'asc' } })
     .then(data => ({ status: 200, data }))
@@ -42,6 +51,14 @@ export const create = async ({ event_id, event_group, name, cpf }: CreatePartici
   const response = client.participant.create({ data: { event_id, event_group, name, cpf } })
     .then(data => ({ status: 201, data }))
     .catch(() => ({ status: 400, data: { error: 'Ocorreu um erro ao cadastrar.' } }))
+
+  return response
+}
+
+export const update = ({ id, event_id, event_group, name, cpf, matched }: UpdateParticipantPayload) => {
+  const response = client.participant.updateMany({ where: { id, event_id, event_group }, data: { name, cpf, matched } })
+    .then(data => ({ status: 200, data }))
+    .catch(() => ({ status: 400, data: { error: "Ocorreu um erro ao atualizar." } }))
 
   return response
 }
