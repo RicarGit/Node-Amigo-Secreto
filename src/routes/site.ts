e9b071ccadd110d6ec04e5654c39042878cd01fb
RicarGit <ricardoagava@gmail.com>
2024-11-12 06:55:33 -0300

    feat: adding public events route

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -1,7 +1,10 @@
 import { Router } from "express"
+import * as events from '../controllers/events'
 
 const router = Router()
 
 router.get('/ping', (_, res) => res.status(200).json({ ping: 'pong' }))
+
+router.get('/events/:id', events.getOneEvent)
 
 export default router
