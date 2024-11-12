6b7f07910094e5a74542549b098828580519986d
RicarGit <ricardoagava@gmail.com>
2024-11-12 06:56:46 -0300

    feat: adding public search participant match route

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import * as events from '../controllers/events'
+import * as participants from '../controllers/participants'
 
 const router = Router()
 
 router.get('/ping', (_, res) => res.status(200).json({ ping: 'pong' }))
 
 router.get('/events/:id', events.getOneEvent)
+router.get('/events/:event_id/search', participants.searchParticipantMatch)
 
 export default router
